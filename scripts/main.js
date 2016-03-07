cdfd4c09602eaad392d271ee6ca8d20095896e7a
ckmah <clarence.iyan@gmail.com>
2016-03-06 22:32:46 -0800

    layout complete, content updated up to dangoreader

--- scripts/main.js
+++ scripts/main.js
@@ -16,67 +16,22 @@
  *  limitations under the License
  *
  */
-var $CARD = $('#info-card');
 
 (function() {
   'use strict';
-
-  // Set initial conditions.
-  init();
-
-  // Show details
-  $('.content-block').click(function() {
-    var $block = $(this);
-
-    // bind data
-    $CARD.find('#title').text($block.data('title'));
-    $CARD.find('#info-pic').css('background-image', $block.data('image'));
-    $CARD.find('#category').text($block.data('category'));
-
-    // Put each tag in its own div
-    $CARD.find('.tag').remove();
-    var tags = String($block.data('tags')).split(',');
-    if (tags[0] !== '') {
-      for (var str of tags) {
-        var div = document.createElement('div');
-        div.innerHTML = str;
-        $(div).addClass('tag');
-        $CARD.find('#category').after(div);
-      }
+  $('a').smoothScroll();
+  $('#carousel-indicators').affix({
+    offset: {
+      top: $('#carousel-indicators').offset().top
     }
-
-    $CARD.find('#description').text($block.data('description'));
-
-    $CARD.fadeIn(300);
   });
-
-  $('#info-close').click(function() {
-    $('#info-card').fadeOut(300);
-  });
-
-  $('.category').hover(function() {
-    var categoryClass = $(this).text();
-    console.log('hover');
-    $('.' + categoryClass).addClass('content-block-hover');
-  }, function(event) {
-    var categoryClass = $(this).text();
-    $('.' + categoryClass).removeClass('content-block-hover');
-  });
-
-})();
-
-/**
- * Set initial conditions on page load.
- */
-function init() {
-  'use strict';
-  $CARD.hide();
   $('#work-carousel').carousel({
     interval: false,
     keyboard: true
   });
-  // particlesJS.load('particles-js', '../particles.json', function() {
-  //   console.log('callback - particles.js config loaded');
-  // });
 
-}
+})();
+
+// particlesJS.load('particles-js', '../particles.json', function() {
+//   console.log('callback - particles.js config loaded');
+// });
